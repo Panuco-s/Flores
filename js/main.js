948b44600c215d8983a7b9d2a1b0b1db60066dbb
Panuco-s <samuel.panuco@uabc.edu.mx>
2025-04-29 11:32:36 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ onload = () => {
     document.body.classList.remove("not-loaded");
 
     // const titles = ('I LOVE U').split('')
-    const titles = ('CHINGA TU MADRE').split('')
+    const titles = ('Hola Mundo').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
