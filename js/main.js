0d5fb7f6dc52b329686fe91ff96557d97ab91da1
Panuco-s <samuel.panuco@uabc.edu.mx>
2026-09-21 17:59:31 -0700

    Change title from 'I LOVE YOU' to 'TQM'

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ onload = () => {
     document.body.classList.remove("not-loaded");
 
     // const titles = ('I LOVE U').split('')
-    const titles = ('I LOVE YOU').split('')
+    const titles = ('TQM').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
