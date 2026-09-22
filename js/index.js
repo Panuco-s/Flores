3745d17e59f3344041e50dd94ceab9ff8e21541c
Panuco-s <samuel.panuco@uabc.edu.mx>
2026-09-21 17:58:56 -0700

    Change text content in index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const title = document.querySelector('.title')
 // const text = 'I Have Something'.split('')
-const text = 'Tengo Algo Para Ti'.split('')
+const text = 'Ojala Pudiera Darte Unas Reales'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
